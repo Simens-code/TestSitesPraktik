be06812b7d6b0aeea79335a1852ea2f4222e8bf4
Семён <semn.kuznecov.06@gmail.com>
2026-04-27 14:24:00 +0500

    Добавлен Page Object Model: base_page, reset_db_page, conftest

---
+++ pages/reset_db_page.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
+
+class ResetDBPage(BasePage):
+    """Страница сброса базы данных (/tests)"""
+    
+    # Локаторы элементов на странице
+    RESET_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
+    ALERT_SUCCESS = (By.CSS_SELECTOR, ".alert-success")
+    
+    def reset_database(self):
+        """Выполнить сброс базы данных"""
+        # Нажимаем кнопку сброса
+        self.click(self.RESET_BUTTON)
+        
+        # Ожидаем появления alert и подтверждаем
+        alert = self.wait.until(EC.alert_is_present())
+        alert.accept()
+        
+        return self
+    
+    def is_reset_successful(self):
+        """Проверить, что сброс прошёл успешно"""
+        return self.is_element_present(self.ALERT_SUCCESS)
